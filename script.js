3cbf48582cf618b2cb0d983557770864f66fab24
khalidtalha00 <117529116+khalidtalha00@users.noreply.github.com>
2024-02-03 11:50:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function submitForm() {
+    // Logic to handle device form submission
+    alert('Device form submitted!'); // Replace this with your actual submission logic
+  }
+  
+  function submitPickup() {
+    // Logic to handle pickup form submission
+    alert('Pickup form submitted!'); // Replace this with your actual submission logic
+  }
+  
